b107a8f948ac5d3d7d30012e521d03925012f0a6
Desmond Kofi Boateng <desmondkofiboateng@GRAD-PHDCOMP03.local>
2024-11-16 20:12:34 -0700

    added small scale licenses for our data

--- license.py
+++ license.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
+license_path = './data/report_license_04112024.xlsx'
+forest_path = './clean_data/forest.csv'
 
-
-license_path='./data/report_license_04112024.xlsx'
-license=pd.read_excel(license_path)
-print(license.head())
+license = pd.read_excel(license_path)
+forest_df = pd.read_csv(forest_path)
 
 def process_license_data(license):
     try:
@@ -45,8 +45,22 @@ def process_license_data(license):
 
     return license_df
 
-license_data=process_license_data(license)
+license_data = process_license_data(license)
 if license_data is not None:
     print(license_data.head())
 
-    
+district_license_count = license_data.groupby('District').size().reset_index(name='Number of Licenses')
+district_license_count.to_csv('./clean_data/district_license_count.csv', index=False)
+
+def get_number_of_licenses(district):
+    try:
+        if (district_license_count['District'] == district).any():
+            return district_license_count.loc[district_license_count['District'] == district, 'Number of Licenses'].iloc[0]
+        else:
+            return None
+    except Exception as e:
+        return None
+district_license_count['District'] = district_license_count['District'].str.replace(r'\s*(District|Region)\s*', '', regex=True)
+forest_df['Number of Licenses'] = forest_df['subnational2'].apply(get_number_of_licenses)
+print(forest_df['Number of Licenses'].nunique())
+
